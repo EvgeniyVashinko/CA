cc21b490da101cd2de0597d42fb92d694ecb4655
EvgeniyVashinko <48275225+EvgeniyVashinko@users.noreply.github.com>
2020-11-16 21:53:29 +0300

    Update lab3.cpp

--- lab3/lab3.cpp
+++ lab3/lab3.cpp
@@ -2,12 +2,14 @@
 #include <omp.h>
 #include <ctime>
 #include <thread>
+#include <vector>
 using namespace std;
 
 int SIZE = 99999999;
 int* a = new int[SIZE];
 int* b = new int[SIZE];
 int result = 0;
+
 int main()
 {
 	chrono::time_point<chrono::steady_clock> time1, time2;
@@ -18,18 +20,24 @@ int main()
 		a[i] = rand() % 100;
 		b[i] = rand() % 100;
 	}
+
 	int i = 0, testRes = 0;;
+
 	time1 = chrono::steady_clock::now();
+
 	for (int i = 0; i < SIZE; i++)
 	{
 		testRes += a[i] * b[i];
 	}
+
 	time2 = chrono::steady_clock::now();
+
 	cout << "TestRes = "<< testRes << endl;
 	cout << "time1 = "<< ((chrono::duration<float>)(time2 - time1)).count() << endl;
 
 	time1 = chrono::steady_clock::now();
-#pragma omp parallel for private(i) shared(a,b) reduction(+:result)
+
+#pragma omp parallel for private(i) shared(a,b) schedule(runtime) reduction(+:result)
 
 	for (i = 0; i < SIZE; i++)
 	{
@@ -37,6 +45,7 @@ int main()
 	}
 
 	time2 = chrono::steady_clock::now();
+
 	cout << "res = " << result << endl;
 	cout << "time2 = " << ((chrono::duration<float>)(time2 - time1)).count() << endl;
 	cout << "result == testRes --- " << (bool)(result == testRes);
